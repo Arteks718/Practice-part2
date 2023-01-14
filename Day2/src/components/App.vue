f5f494fe74fa40fd8509a40f8e3c6474a40360ea
Arteks718 <artexbro.official@gmail.com>
2023-01-14 20:45:05 +0200

    update day2

--- Day2/src/components/App.vue
+++ Day2/src/components/App.vue
@@ -1,36 +1,76 @@
 <template>
   <div id="app">
-    <input type="text" name="search" v-model="search" /><br />
     <table class="table table-dark">
+      <tr>
+        <th>ПІБ</th>
+        <th>Наявність ПР</th>
+        <th>Група</th>
+        <th>Оновлення</th>
+        <th>Видалення</th>
+      </tr>
       <tr v-for="(item, index) in students" v-bind:key="item._id">
         <td>{{ item.name }}</td>
         <td><input type="checkbox" v-model="item.isDonePr" /></td>
         <td>{{ item.group }}</td>
-        <td>
-          <i class="fas fa-edit" @click="updateShow(item._id, index)"></i>
+        <td class="update">
+          <i class="fas fa-edit" @click="updateShow(item._id, index), openBlockofUpdate()"></i>
         </td>
-        <td><a href="#" @click="deleteStudent(item._id)">Видалити</a></td>
+        <td class="delete">
+          <a href="#" @click="deleteStudent(item._id)">Видалити</a>
+        </td>
       </tr>
     </table>
-    <section>
-      <input v-model="student.name" />
-      <input type="checkbox" v-model="student.isDonePr" />
-      <select v-model="student.group">
-        <option value="RPZ 19 1/9">RPZ 19 1/9</option>
-        <option value="RPZ 19 2/9">RPZ 19 2/9</option>
-      </select>
-      <button @click="addStudent()">Add student</button>
-    </section>
 
-    <section>
-      <input v-model="tempStudents.name" />
-      <input type="checkbox" v-model="tempStudents.isDonePr" />
-      <select v-model="tempStudents.group">
-        <option value="RPZ 19 1/9">RPZ 19 1/9</option>
-        <option value="RPZ 19 2/9">RPZ 19 2/9</option>
-      </select>
-      <button @click.prevent="updateStudent()">Update student</button>
-    </section>
+    <div class="functions">
+      <div class="student">
+        <button id="openBlock" @click="openBlockofAdd()">
+          Додати нового студента
+        </button>
+        <div class="addStudent">
+          <div>
+            <label>ПІБ студента</label>
+            <input v-model="student.name" />
+          </div>
+          <div>
+            <label for="pr">Наявність ПР</label>
+            <input type="checkbox" v-model="student.isDonePr" />
+          </div>
+          <div>
+            <label for="group">Група</label>
+            <select v-model="student.group">
+              <option value="RPZ 19 1/9">RPZ 19 1/9</option>
+              <option value="RPZ 19 2/9">RPZ 19 2/9</option>
+            </select>
+          </div>
+          <button @click="addStudent()">Добавити</button>
+        </div>
+      </div>
+      <div class="student">
+        <button id="openBlock" @click="openBlockofUpdate()">
+          Оновити студента
+        </button>
+        <div class="updateStudent">
+          <div>
+            <label>ПІБ студента</label>
+            <input v-model="tempStudents.name" />
+          </div>
+          <div>
+            <label for="pr">Наявність ПР</label>
+            <input type="checkbox" v-model="tempStudents.isDonePr" />
+          </div>
+          <div>
+            <label for="group">Група</label>
+            <select v-model="tempStudents.group">
+              <option value="RPZ 19 1/9">RPZ 19 1/9</option>
+              <option value="RPZ 19 2/9">RPZ 19 2/9</option>
+            </select>
+          </div>
+          <button @click.prevent="updateStudent()">Оновити</button>
+        </div>
+      </div>
+    </div>
+
+    <section></section>
 
     <section class="exchange">
       <h2>Currency Converter</h2>
@@ -78,6 +118,7 @@ export default {
       convertTo: "",
       amount: "",
       rate: "",
+      search: "",
     };
   },
   mounted() {
@@ -86,13 +127,15 @@ export default {
     });
   },
   methods: {
-    addStudent() {
+    addStudent(element) {
       axios
         .post("http://34.82.81.113:3000/students", {
           ...this.student,
         })
         .then((response) => {
           this.students.push(response.data);
+          element = document.querySelector(".addStudent");
+          element.style.display = "none";
         });
     },
     deleteStudent(studId) {
@@ -110,7 +153,7 @@ export default {
       };
       this.tempIndex = index;
     },
-    updateStudent() {
+    updateStudent(element) {
       axios
         .put(`http://34.82.81.113:3000/students/${this.tempStudents._id}`, {
           ...this.tempStudents,
@@ -118,6 +161,8 @@ export default {
         .then((response) => {
           this.students[this.tempIndex] = response.data;
           this.tempStudents = [];
+          element = document.querySelector(".updateStudent");
+          element.style.display = "none";
         });
     },
     getCurrency() {
@@ -145,6 +190,14 @@ export default {
             " " +
             this.convertTo;
         });
+    },
+    openBlockofAdd: function (element) {
+      element = document.querySelector(".addStudent");
+      element.style.display = "block";
+    },
+    openBlockofUpdate: function (element) {
+      element = document.querySelector(".updateStudent");
+      element.style.display = "block";
     },
   },
 };
